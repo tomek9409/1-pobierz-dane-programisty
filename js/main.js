751b7ca83cd9786ced9b39dbb2867e6c07e322ae
tomek9409 <kolpak9409@gmail.com>
2022-09-30 13:45:33 +0200

    x

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(function () {
   $("#dane").click(function () {
     $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php")
         .done(function (data) {
-          $(info).after(`<br><div>Imię: ${data.imie}<br> Nazwisko: ${data.nazwisko}<br> Zawód: ${data.zawod}<br> Firma: ${data.firma} <br> <hr/>`);   
+          $(info).append(`<br>Imię: ${data.imie}<br> Nazwisko: ${data.nazwisko}<br> Zawód: ${data.zawod}<br> Firma: ${data.firma} <br> <hr/>`);   
    })
   })
 })
